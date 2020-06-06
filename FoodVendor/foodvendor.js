1831d8c737be7d5a8f91e3e8716cb9a993cc233f
jonahrosenblum <jonaherosenblum@gmail.com>
2020-06-06 03:24:56 +0000

    update vendor endpoint to handle errors

--- FoodVendor/foodvendor.js
+++ FoodVendor/foodvendor.js
@@ -1,23 +1,23 @@
 'use strict'
 
-const tracing = require('@opencensus/nodejs');
-const propagation = require('@opencensus/propagation-b3');
+// const tracing = require('@opencensus/nodejs');
+// const propagation = require('@opencensus/propagation-b3');
 
-// Creates Zipkin exporter
-const zipkin = require('@opencensus/exporter-zipkin');
-const exporter = new zipkin.ZipkinTraceExporter({
-  url: 'http://localhost:9411/api/v2/spans',
-  serviceName: 'foodvendor'
-});
+// // Creates Zipkin exporter
+// const zipkin = require('@opencensus/exporter-zipkin');
+// const exporter = new zipkin.ZipkinTraceExporter({
+//   url: 'http://localhost:9411/api/v2/spans',
+//   serviceName: 'foodvendor'
+// });
 
-// NOTE: Please ensure that you start the tracer BEFORE initializing express app
-// Starts tracing and set sampling rate, exporter and propagation
-tracing.start({
-  exporter,
-  samplingRate: 1, // For demo purposes, always sample
-  propagation: new propagation.B3Format(),
-  logLevel: 1 // show errors, if any
-});
+// // NOTE: Please ensure that you start the tracer BEFORE initializing express app
+// // Starts tracing and set sampling rate, exporter and propagation
+// tracing.start({
+//   exporter,
+//   samplingRate: 1, // For demo purposes, always sample
+//   propagation: new propagation.B3Format(),
+//   logLevel: 1 // show errors, if any
+// });
 
 const express = require('express')
 const fs = require('fs')
@@ -37,13 +37,26 @@ const PORT = 7502
 const HOST = '127.0.0.1'
 
 const foodVendor = (req, res) => {
+  if (!req.query) {
+    res.send({})
+    return
+  }
+  const vendor = req.query.vendorQuery
+  const ingredient = req.query.ingredientQuery
+  if (!vendor || !ingredient) {
+    res.send({})
+    return
+  }
   const randomDelay = Math.floor(Math.random() * 50)
-
   setTimeout(()=>{
-    const vendorInfo = vendorsList[req.query.vendorQuery][req.query.ingredientQuery]
-    vendorInfo['vendorName'] = req.query.vendorQuery
-
-    res.send(vendorInfo)
+    try {
+      const vendorInfo = vendorsList[req.query.vendorQuery][req.query.ingredientQuery]
+      vendorInfo['vendorName'] = req.query.vendorQuery
+      res.send(vendorInfo)
+    }
+    catch(error) {
+      res.send({})
+    }
   }, randomDelay)
 }
 
