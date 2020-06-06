629decf8e6639f2f707ee00de2f636a8c03f2d23
jonahrosenblum <jonaherosenblum@gmail.com>
2020-06-06 00:53:09 +0000

    update Docker and deployment for supplier

--- FoodSupplier/foodsupplier.js
+++ FoodSupplier/foodsupplier.js
@@ -1,23 +1,23 @@
 'use strict'
 
-const tracing = require('@opencensus/nodejs');
-const propagation = require('@opencensus/propagation-b3');
+// const tracing = require('@opencensus/nodejs');
+// const propagation = require('@opencensus/propagation-b3');
 
-// Creates Zipkin exporter
-const zipkin = require('@opencensus/exporter-zipkin');
-const exporter = new zipkin.ZipkinTraceExporter({
-  url: 'http://localhost:9411/api/v2/spans',
-  serviceName: 'foodsupplier'
-});
+// // Creates Zipkin exporter
+// const zipkin = require('@opencensus/exporter-zipkin');
+// const exporter = new zipkin.ZipkinTraceExporter({
+//   url: 'http://localhost:9411/api/v2/spans',
+//   serviceName: 'foodsupplier'
+// });
 
-// NOTE: Please ensure that you start the tracer BEFORE initializing express app
-// Starts tracing and set sampling rate, exporter and propagation
-tracing.start({
-  exporter,
-  samplingRate: 1, // For demo purposes, always sample
-  propagation: new propagation.B3Format(),
-  logLevel: 1 // show errors, if any
-});
+// // NOTE: Please ensure that you start the tracer BEFORE initializing express app
+// // Starts tracing and set sampling rate, exporter and propagation
+// tracing.start({
+//   exporter,
+//   samplingRate: 1, // For demo purposes, always sample
+//   propagation: new propagation.B3Format(),
+//   logLevel: 1 // show errors, if any
+// });
 
 const express = require('express')
 const fs = require('fs')
@@ -34,9 +34,12 @@ fs.readFile('suppliers.json', 'utf8', function (err,data) {
 
 // Constants
 const PORT = 7501
-const HOST = '127.0.0.1'
 
 const foodSupplier = (req, res) => {
+  if (!req.query || !req.query.ingredientQuery) {
+    res.send([])
+    return
+  }
   res.send(suppliersList[req.query.ingredientQuery])
 }
 
@@ -46,5 +49,5 @@ app.use(express.urlencoded({ extended: false }));
 
 app.get(`/FoodSupplier`, foodSupplier)
 
-app.listen(PORT, HOST)
-console.log(`Running on http://${HOST}:${PORT}/FoodSupplier`)
+app.listen(PORT, () => {console.log('Running FoodSupplier')})
+//console.log(`Running on http://${HOST}:${PORT}/FoodSupplier`)
